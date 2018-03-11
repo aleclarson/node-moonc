c0c4e3d6cbfbb6b2420693ae42c96948e08f5e99
aleclarson <alec.stanford.larson@gmail.com>
2018-03-11 19:34:05 -0400

    wrap spawn call with setImmediate
    This allows event listeners to be attached before the process is spawned.

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const {spawn} = require('child_process')
 
 function moonc(input) {
   let thru = new PassThrough()
-  let proc = spawn('moonc', ['--'])
-  proc.stdout.pipe(thru)
-  proc.stderr.on('data', (data) => {
-    let err = data.toString()
-    err = err.slice(err.indexOf('Failed'))
-    err = '  ' + err.trim().replace(/\n\s+/g, '\n    ')
-    thru.emit('error', new SyntaxError(err))
+  setImmediate(() => {
+    let proc = spawn('moonc', ['--'])
+    proc.stdout.pipe(thru)
+    proc.stderr.on('data', (data) => {
+      let err = data.toString()
+      err = err.slice(err.indexOf('Failed'))
+      err = '  ' + err.trim().replace(/\n\s+/g, '\n    ')
+      thru.emit('error', new SyntaxError(err))
+    })
+    proc.once('close', () => thru.end())
+    proc.once('error', (err) => thru.emit('error', err))
+    if (typeof input == 'string') {
+      proc.stdin.write(input)
+      proc.stdin.end()
+    } else {
+      input.pipe(proc.stdin)
+    }
   })
-  proc.once('close', () => thru.end())
-  proc.once('error', (err) => thru.emit('error', err))
-  if (typeof input == 'string') {
-    proc.stdin.write(input)
-    proc.stdin.end()
-  } else {
-    input.pipe(proc.stdin)
-  }
   return thru
 }
 
