944465051701eab7eff3ec07e873e0a620d9eab2
aleclarson <alec.stanford.larson@gmail.com>
2018-03-16 17:12:33 -0400

    update moonc process code

--- moon.lua
+++ moon.lua
@@ -1,12 +1,19 @@
 local moon = require('moonscript.base')
+
+local SEP = '\n{++}\n'
+local EOF = '\n{--}\n'
+
 while true do
-  -- The length and payload are separated with a space.
-  local len, sep = io.read('*n', 1)
-  local lua, val = moon.to_lua(io.read(len))
-  if lua == nil then
-    io.stderr:write(val)
-  else
-    io.write(lua .. '\r\n[' .. table.concat(val, ',') .. ']')
+  local len = tonumber(io.read('*l'))
+  if len then
+    local lua, val = moon.to_lua(io.read(len))
+    if lua then
+      if val[1] == nil then val = {} end
+      io.write(lua .. SEP .. '[' .. table.concat(val, ',') .. ']' .. EOF)
+    else
+      -- Signal an error.
+      io.write(SEP .. val .. EOF)
+    end
   end
   io.flush()
 end
